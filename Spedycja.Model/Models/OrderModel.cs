a57128691225a63550bc4196d16f87ada3573763
miki9938 <miki9938@gazeta.pl>
2014-06-02 15:56:12 +0200

    new order model

--- Spedycja.Model/Models/OrderModel.cs
+++ Spedycja.Model/Models/OrderModel.cs
@@ -1,24 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
-namespace Spedycja.Site.Models
+
+namespace Spedycja.Model.Models
 {
     public class OrderModel
     {//update2
+
+        [Display(Name = "Nazwa ładunku")]
+        [Required]
         public string CargoName { get; set; }
 
+        [Display(Name = "Waga")]
         public int Weight { get; set; }
 
+        [Display(Name = "Objętość")]
         public double Volume { get; set; }
 
+        [Display(Name = "Typ ładunku")]
         public int CargoType { get; set;}
 
+        [Display(Name = "Cena")]
         public decimal Price { get; set; }
 
+        [Display(Name = "Typ pojazdu")]
         public int VehicleType { get; set; }
 
+        [Required]
+        [HiddenInput(DisplayValue = true)]
         public int Status { get; set; }
 
         public string StartCountry { get; set; }
@@ -33,10 +47,14 @@ namespace Spedycja.Site.Models
 
         public string EndStreet { get; set; }
 
+        [Required]
+        [HiddenInput(DisplayValue = false)]
         public string WorkerLogin { get; set; }
 
+        [HiddenInput(DisplayValue = false)]
         public int? CustomerId { get; set; }
 
+        [HiddenInput(DisplayValue = false)]
         public int? DriverId { get; set; }
     }
 }
