23d25da18ed7f2ac017a142e35cca700e108e277
bartek.kaaz <bartek47@gmail.com>
2014-06-08 23:27:26 +0200

    update chrome extension

--- chromeExtension/content.js
+++ chromeExtension/content.js
@@ -1,33 +1,47 @@
-$(".contentRight").append("<button class='injection-button'>Wyślij dane z rozszerzenia</button>")
-
-var getFormValue = function(name, values, child){
+var getFormValue = function(name, values){
 	var returnValue;
 	values.each(function(i, value){
 		if(!_.str.startsWith($(value).html(), name))
 			return;
-		if(child === "undefined" || child === null)
-			returnValue = $(values[i+1]).text();
-		else
-			returnValue = $(values[i+1]).children(child).text();
+		returnValue = $(values[i+1]).text();
 	})
 	return returnValue;
 }
 
-var values = $(".contentRight tbody td p")
-
-var data= {
-	Type: getFormValue("Rodzaj", values),
-	Weight: getFormValue("Ciężar", values),
-	Volume: getFormValue("Objętość", values),
-	Price: getFormValue("Proponowana cena", values),
-	VehicleType: getFormValue("Naczepa", values),
-	From: getFormValue("Transport", values, "b"),
-	End: getFormValue("Transport", values, "font")
+var getFormChildValue = function(name, values, child){
+	var returnValue;
+	values.each(function(i, value){
+		if(!_.str.startsWith($(value).html(), name))
+			return;
+		returnValue = $(values[i+1]).children(child).text();
+	})
+	return returnValue;
 }
 
-$(".injection-button").click(function(){
-	chrome.runtime.sendMessage({data: data}, function(response) {
-	  console.log(response.ACK);
+$(document).ready(function(){
+	$(".contentRight").append("<button class='injection-button'>Wyślij dane z rozszerzenia</button>");
+
+	var values = $(".contentRight tbody td p");
+
+	var data= {
+		LoadName: $(".contentRight tbody h3").text(),
+		Type: getFormValue("Rodzaj", values),
+		Weight: getFormValue("Ciężar", values),
+		Volume: getFormValue("Objętość", values),
+		Price: getFormValue("Proponowana cena", values),
+		VehicleType: getFormValue("Naczepa", values),
+		From: getFormChildValue("Transport", values, "b"),
+		End: getFormChildValue("Transport", values, "font"),
+		ClientName: getFormValue("Imię/nazwisko", values),
+		ClientCompany: getFormValue("Firma", values),
+		ClientPhone: getFormValue("Telefon", values),
+		ClientAdress: getFormValue("Adres", values)	
+	}
+
+	$(".injection-button").click(function(){
+		chrome.runtime.sendMessage({data: data}, function(response) {
+		  console.log(response.ACK);
+		});
 	});
 });
 
--- chromeExtension/injectForm.js
+++ chromeExtension/injectForm.js
@@ -1,12 +1,16 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        $("#oType").val(request.data.Type);
-        $("#oWeight").val(parseFloat(request.data.Weight));
-        $("#oVolume").val(parseFloat(request.data.Volume));
-        $("#oPrice").val(parseFloat(request.data.Price));
-        $("#lVehicleType").val(request.data.VehicleType);
-        $("#oStartPoint").val(request.data.From);
-        $("#oEndPoint").val(request.data.End);
+        $("#load_Name").val(request.data.LoadName);
+        $("#load_LoadType").val(request.data.Type);
+        $("#load_Weight").val(parseFloat(request.data.Weight));
+        $("#load_Price").val(parseFloat(request.data.Price));
+        $("#vehicle_Name").val(request.data.VehicleType);
+        $("#route_StartPoint").val(request.data.From);
+        $("#route_EndPoint").val(request.data.End);
+        $("#customer_Name").val(request.data.ClientName);
+        $("#customer_Address").val(request.data.End);
+        $("#customer_PhoneNumber").val(request.data.ClientPhone);
+        $("#customer_Firm").val(request.data.ClientCompany);
 
         sendResponse({ACK: true});
     }
